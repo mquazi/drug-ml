4e5be38f54dff6e276371afeaf792faa54c0e352
Mahmudulla Hassan <mhassan@miners.utep.edu>
2023-08-10 01:19:51 +0000

    reformat

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -6,4 +6,24 @@ app = Flask(__name__)
 def index():
     title = 'My Flask Website'
     website_name = 'Awesome Flask Site'
-    return render_template('index.html', title=title, website_name=website_name)
+    return render_template('index.html')
+
+@app.route("/structure")
+def structure():
+    return render_template('structure.html')
+
+@app.route("/batch")
+def batch():
+    return render_template('batch.html')
+
+@app.route("/models")
+def models():
+    return render_template('models.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True)
